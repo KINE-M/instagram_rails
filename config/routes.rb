0e0c4f0f58d02a70b2fb228934c4448c8a12868c
KINE-M <a.m.cassandre.k@gmail.com>
2020-07-28 23:45:48 +0900

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :photos
-  resources :users
+  resources :users do
+    resource :follow
+    resources :followers
+    resources :followings
+  end
 end
