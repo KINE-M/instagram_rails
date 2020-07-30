c6ee099d6e56a4e162cc51ee76095568799bb7b1
KINE-M <a.m.cassandre.k@gmail.com>
2020-07-30 20:13:57 +0900

    feature: Create comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: 'Follow', foreign_key: 'target_user_id'
   has_many :followings, through: :active_relationships, source: :target_user
   has_many :followers, through: :passive_relationships, source: :user
+  has_many :comments
 end
